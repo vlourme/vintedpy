91d6596caed55ac6e693287131deba7d168a2a3f
Victor Lourme <victor.lourme@icloud.com>
2023-05-12 18:04:06 +0200

    fix(scraper): catch all KeyError

--- scraper.py
+++ scraper.py
@@ -18,14 +18,11 @@ def scrape(db: Database, params: Dict[str, str]) -> List:
     Returns:
         List: list of new items
     """
-    response = search(params['url'], {
-        'per_page': 20
-    })
+    response = search(params["url"], {"per_page": 20})
 
     # Remove promoted items
     try:
-        items = [item for item in response['items']
-                 if item['promoted'] == False]
+        items = [item for item in response["items"] if item["promoted"] == False]
     except KeyError:
         return []
 
@@ -34,35 +31,35 @@ def scrape(db: Database, params: Dict[str, str]) -> List:
         return []
 
     # Ignore items for first sync
-    if params['last_sync'] == -1:
+    if params["last_sync"] == -1:
         return [items[0]]
 
-    table = db['items']
+    table = db["items"]
 
     # Filter date and by existing
     results = []
     for item in items:
         try:
-            timestamp = item['photo']['high_resolution']['timestamp']
-        except KeyError:
+            timestamp = item["photo"]["high_resolution"]["timestamp"]
+        except:
             log.warning("Empty timestamp found")
             print(item)
             continue
 
-        if timestamp > params['last_sync'] and 'id' in item:
+        if timestamp > params["last_sync"] and "id" in item:
             results.append(item)
 
     for item in results:
-        saved = table.find_one(id=item['id'])
+        saved = table.find_one(id=item["id"])
         log.debug(saved)
 
         if saved:
             # Already known
-            log.debug("Removing result {id}, already known", id=item['id'])
+            log.debug("Removing result {id}, already known", id=item["id"])
             results.remove(item)
         else:
-            log.debug("Inserting item #{id}", id=item['id'])
-            table.insert({'id': item['id']})
+            log.debug("Inserting item #{id}", id=item["id"])
+            table.insert({"id": item["id"]})
 
     return results
 
@@ -79,18 +76,20 @@ def generate_embed(item: Any, sub_id: int) -> hikari.Embed:
         hikari.Embed: Generated embed
     """
     embed = hikari.Embed()
-    embed.title = item['title'] or "Unknown"
-    embed.url = item['url'] or "Unknown"
-    embed.set_image(item['photo']['url'] or "Unknown")
-    embed.color = hikari.Color(0x09b1ba)
-    embed.add_field('Price', str(item['price']) or "-1" + ' €', inline=True)
-    embed.add_field('Size', item['size_title'] or "-1", inline=True)
+    embed.title = item["title"] or "Unknown"
+    embed.url = item["url"] or "Unknown"
+    embed.set_image(item["photo"]["url"] or "Unknown")
+    embed.color = hikari.Color(0x09B1BA)
+    embed.add_field("Price", str(item["price"]) or "-1" + " €", inline=True)
+    embed.add_field("Size", item["size_title"] or "-1", inline=True)
 
     date = datetime.utcfromtimestamp(
-        int(item['photo']['high_resolution']['timestamp'])).strftime('%d/%m/%Y, %H:%M:%S')
-    embed.set_footer(
-        f'Published on {date or "unknown"} • Subscription #{str(sub_id)}')
-    embed.set_author(name='Posted by ' + item['user']['login'] or "unknown",
-                     url=item['user']['profile_url'] or "unknown")
+        int(item["photo"]["high_resolution"]["timestamp"])
+    ).strftime("%d/%m/%Y, %H:%M:%S")
+    embed.set_footer(f'Published on {date or "unknown"} • Subscription #{str(sub_id)}')
+    embed.set_author(
+        name="Posted by " + item["user"]["login"] or "unknown",
+        url=item["user"]["profile_url"] or "unknown",
+    )
 
     return embed
